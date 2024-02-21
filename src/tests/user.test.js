8cf6f2b8aaddda30a24cfda088e385934b3a44a5
Ivan Aguirre <viac321@gmail.com>
2024-02-20 22:03:27 -0600

    user.test updated

--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -2,6 +2,8 @@ const request = require('supertest');
 const app = require('../app');
 
 const URL_BASE = '/users'
+let TOKEN
+let userID
 
 const user = {
     firstName: 'test',
@@ -11,12 +13,69 @@ const user = {
     phone: 'test'
 }
 
+beforeAll(async() => {
+    const user = {
+        email: 'test',
+        password: 'test'
+    }
+    const res = await request(app)
+    .post(`${URL_BASE}/login`)
+    .send(user)
+
+    TOKEN = res.body.token
+})
+
+test("'GET -> 'URL_BASE', should return status code 201, r ", async() => { 
+    const res = await request(app)
+    .get(URL_BASE)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body).toHaveLength(1)
+ })
+
 test("'POST -> 'URL_BASE', should return status code 201, r ", async() => { 
     const res = await request(app)
     .post(URL_BASE)
     .send(user)
 
+    userID = res.body.id
+
     expect(res.statusCode).toBe(201)
     expect(res.body).toBeDefined()
     expect(res.body.firstName).toEqual(user.firstName)
  })
+
+test ("'PUT -> 'URL_BASE', should return status code 201, r ", async() => {
+    const res = await request(app)
+    .put(`${URL_BASE}/${userID}`)
+    .send({ firstName: 'test2' })
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.firstName).toEqual('test2')
+}) 
+
+test("POST -> 'URL_BASE/:id', should return status code 204, r ", async() => {
+    const res = await request(app)
+    .delete(`${URL_BASE}/${userID}`)
+    .set('Authorization', `Bearer ${TOKEN}`)
+})
+
+test("POST-> 'URL_BASE/login', should return status code 201, r ", async() => {
+    const userLogin = {
+        email: 'test',
+        password: 'test'
+    }
+    const res = await request(app)
+    .post(`${URL_BASE}/login`)
+    .send(userLogin)
+    .set('Authorization', `Bearer ${TOKEN}`)
+
+    expect(res.statusCode).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.user.email).toBe(userLogin.email)})
+    expect(res.body.TOKEN).toBeDefined()
+ 
