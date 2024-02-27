b4cff5ff1b2d573da60e31e7ae746decb7e4a7a1
Ivan Aguirre <viac321@gmail.com>
2024-02-27 13:21:30 -0600

    prductimgs and product update

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -48,11 +48,24 @@ const update = catchError(async(req, res) => {
     return res.json(result[1][0]);
 });
 
+const setImages = catchError(async(req,res) =>{
+    //route -> /products/:id/images
+    const {id} = req.params
+    const product = await Product.findByPk(id)
+    if(!product) return res.sendStatus(404)
+
+    await product.setProductImgs(req.body)
+    const images = await product.getProductImgs()
+
+    return res.json(images)
+})
+
 
 module.exports = {
     getAll,
     create,
     getOne,
     remove,
-    update
+    update,
+    setImages
 }
--- src/routes/productImg.route.js
+++ src/routes/productImg.route.js
@@ -1,11 +1,14 @@
-const { getAll } = require('../controllers/productImg.controller');
+const { getAll, create, remove } = require('../controllers/productImg.controller');
 const express = require('express');
 const upload = require('../utils/multer');
 
 const productImg = express.Router();
 
-productImg.route('/')
+routerproductImg.route('/')
     .get(getAll)
-    .post(upload.single('image'))
-
+    .post(upload.single('image'), create)
+    
+routerProductImg.route('/:id')
+    .delete(remove)    
+    
 module.exports = productImg;
